51c4c0ca4d3163c9f87357fdbec00276eb6c988d
Jivago Alves <jivagoalves@gmail.com>
2023-03-05 12:20:33 +0100

    Remove unused extension fn for LocalDate

--- application/src/main/kotlin/com/gildedrose/extensions.kt
+++ application/src/main/kotlin/com/gildedrose/extensions.kt
@@ -8,9 +8,6 @@ import kotlin.time.DurationUnit
 internal val Int.day: Duration
     get() = this.days
 
-internal operator fun LocalDate.minus(days: Duration): LocalDate =
-    minusDays(days.toLong(DurationUnit.DAYS))
-
 internal operator fun LocalDate.plus(days: Duration): LocalDate =
     if (this == LocalDate.MAX) LocalDate.MAX
     else this.plusDays(days.toLong(DurationUnit.DAYS))
