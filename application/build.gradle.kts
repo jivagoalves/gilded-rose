bd966116b3753bf410cb9772636ac0ca985fcc77
Jivago Alves <jivagoalves@gmail.com>
2023-06-18 21:48:33 +0200

    Add mutation testing

--- application/build.gradle.kts
+++ application/build.gradle.kts
@@ -1,7 +1,10 @@
+
+import info.solidsoft.gradle.pitest.PitestPluginExtension
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     kotlin("jvm") version "1.8.10"
+    id("info.solidsoft.pitest") version "1.9.11"
 }
 
 group = "com.gilded-rose"
@@ -13,10 +16,13 @@ repositories {
 }
 
 val arrowVersion = "1.1.5"
+val pitestJunit5Plugin = "1.1.2"
 
 dependencies {
     implementation("io.arrow-kt:arrow-core:$arrowVersion")
     implementation("org.junit.jupiter:junit-jupiter:5.9.2")
+    pitest("org.pitest:pitest-junit5-plugin:$pitestJunit5Plugin")
+    pitest("com.groupcdg.pitest:pitest-kotlin-plugin:1.0.7")
 
     // testing
     testImplementation(kotlin("test"))
@@ -32,4 +38,17 @@ tasks.withType<KotlinCompile> {
 
 tasks.withType<Test> {
     useJUnitPlatform()
+}
+
+configure<PitestPluginExtension> {
+    pitestVersion.set("1.14.1")
+    junit5PluginVersion.set(pitestJunit5Plugin)
+    avoidCallsTo.set(setOf("kotlin.jvm.internal"))
+    mutators.set(setOf("STRONGER"))
+    targetClasses.set(setOf("com.gildedrose.*"))
+    targetTests.set(setOf("com.gildedrose.*"))
+    threads.set(Runtime.getRuntime().availableProcessors())
+    outputFormats.set(setOf("XML", "HTML"))
+    mutationThreshold.set(75)
+    coverageThreshold.set(60)
 }
